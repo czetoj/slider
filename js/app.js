a630ae4e03467eb70fff1c9bb0368501ae92d13f
czetoj <czeto.jozsef.0229@gmail.com>
2020-12-06 16:22:34 +0100

    individual and automatic slideshow

---
+++ js/app.js
@@ -0,0 +1,101 @@
+import pictures from './pictures.js';
+
+const sliderBox = document.querySelector('.slider-box');
+const leftArrow = document.querySelector('.left-arrow');
+const rightArrow = document.querySelector('.right-arrow');
+const dots = document.querySelector('.dots');
+let index = 0;
+let auto = true;
+
+sliderIndivudualAndAutomatic(3000, 90)
+
+
+function sliderIndivudualAndAutomatic(frequency, sliderViewportHeight) {
+
+    let timer = frequency + 1000;
+    loadPictures();
+    const slider = document.querySelectorAll('.slider');
+    showDots();
+    const dot = document.querySelectorAll('.dot');
+    showPictureAuto();
+    const showAuto = setInterval(showPictureAuto, frequency);
+
+    setInterval(() => {
+        if (!auto) {
+            timer += 1000;
+            showPictureAuto();
+        }
+    }, frequency)
+
+    leftArrow.addEventListener('click', () => {
+        auto = false;
+        timer = 0;
+        clearInterval(showAuto);
+        showPicture(index - 1);
+    });
+
+    rightArrow.addEventListener('click', () => {
+        auto = false;
+        timer = 0;
+        clearInterval(showAuto);
+        showPicture(index + 1);
+    });
+
+    for (let i = 0; i < pictures.length; i++) {
+        dot[i].addEventListener('click', () => {
+            auto = false;
+            timer = 0;
+            clearInterval(showAuto);
+            showPicture(i);
+        });
+    }
+
+    function showDots() {
+        for (let i = 0; i < pictures.length; i++) {
+            const dotElem = document.createElement('span');
+            dotElem.classList.add('dot');
+            dots.prepend(dotElem);
+        }
+    }
+
+    function showPicture(i) {
+        dot.forEach(item => item.classList.remove('dot-showed'));
+        slider[index].classList.remove('show')
+        slider[index].classList.add('hide');
+        if (i >= pictures.length) i = 0;
+        if (i < 0) i = pictures.length - 1;
+        slider[i].classList.remove('hide');
+        slider[i].classList.add('show');
+        dot[i].classList.add('dot-showed');
+        index = i;
+    }
+
+    function showPictureAuto() {
+        if (timer > 3000) {
+            dot.forEach(item => item.classList.remove('dot-showed'));
+            slider[index].classList.remove('show')
+            slider[index].classList.add('hide');
+            index++;
+            if (index >= pictures.length) index = 0;
+            if (index < 0) index = pictures.length - 1;
+            slider[index].classList.remove('hide');
+            slider[index].classList.add('show');
+            dot[index].classList.add('dot-showed');
+        }
+    }
+
+    function loadPictures() {
+        for (let i = 0; i < pictures.length; i++) {
+            const sliderElement = document.createElement('div');
+            sliderElement.classList.add('slider');
+            sliderElement.classList.add('hide');
+            sliderElement.style.height = `${sliderViewportHeight}vh`;
+            sliderElement.innerHTML = `
+                                            <div class="count">${i + 1} / ${pictures.length}</div>
+                                            <img src=${pictures[i].url} alt=${pictures[i].alt}>
+                                            <div class="caption">${pictures[i].caption}</div>
+                                        `;
+            sliderBox.append(sliderElement);
+        }
+    }
+}
---
+++ js/pictures.js
@@ -0,0 +1,35 @@
+const pictures = [
+    {
+        url: './images/1.jpg',
+        caption: 'Opatija, tengerparti sétány',
+        alt: 'Opatija, tengerparti sétány'
+    },
+    {
+        url: './images/2.jpg',
+        caption: 'Vrsar, Isztria',
+        alt: 'Vrsar, Isztria'
+    },
+    {
+        url: './images/3.jpg',
+        caption: 'Kilátás az omisi erődből',
+        alt: 'Kilátás az omisi erődből'
+    },
+    {
+        url: './images/4.jpg',
+        caption: 'Krka vízesés',
+        alt: 'Krka vízesés'
+    },
+    {
+        url: './images/5.jpg',
+        caption: 'Omis, kilátás a Mirabellből',
+        alt: 'Omis, kilátás a Mirabellből'
+    },
+    {
+        url: './images/6.jpg',
+        caption: 'Cesky Krumlov',
+        alt: 'Cesky Krumlov'
+    },
+]
+
+
+export default pictures;
